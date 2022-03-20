c2dbb62a9d1c7da447a531bbe84449a3114707a3
ClintZy101 <clielizek.joy@gmail.com>
2022-03-21 00:11:43 +0900

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, TouchableNativeFeedback, TouchableHighlight, Button, Alert } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+     console.log('text clicked')
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+     
+      <Text style={styles.text}> Hello Native! </Text>
+      {/* <Image source={require("./public/somephoto.png")}/> */}
+      
+      <TouchableHighlight onPress={()=>console.log('image tapped')}>
+      <Image style={styles.logo} source={{uri: "https://picsum.photos/200/300"}}/>
+      </TouchableHighlight>
+
+      {/* ALert Button */}
+      <Button title="Alert Button" color="green" onPress={()=>Alert.alert('My Title', 'My Message', [{text: 'Yes', onPress: ()=> console.log('Yes')}, {text: 'No', onPress: ()=> console.log('No')}])}/>
+
+      {/* Prompt Button */}
+      <Button title="Prompt Button" color="blue" onPress={()=>Alert.prompt('My Title', 'My Message', text => console.log(text))}/>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +28,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+
   },
+  text :{
+    marginBottom: 20
+  },
+  logo : {
+    height: 400,
+    width: 400,
+   
+  }
 });
